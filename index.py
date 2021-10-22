ac0cc6dd671c45219356e21853c595f317127ecc
LucioPruneda <lucio_pruneda@hotmail.com>
2021-10-22 17:16:11 -0300

    Inicializando

--- index.py
+++ index.py
@@ -2,3 +2,17 @@ from tkinter import ttk
 from tkinter import *
 import sqlite3
 
+class Gastos:
+    
+
+    def __init__(self, window):
+        # Inicializacion 
+        self.wind = window
+        self.wind.title('Aplicacion de Gastos')
+
+
+
+if __name__ == '__main__':
+    window = Tk()
+    application = Gastos(window)
+    window.mainloop()
