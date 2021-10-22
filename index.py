35c85cf8e0a14f501d37e05c68c34f7d37488dfa
LucioPruneda <lucio_pruneda@hotmail.com>
2021-10-22 17:24:32 -0300

    Frame and label

--- index.py
+++ index.py
@@ -11,6 +11,18 @@ class Gastos:
         self.wind.title('Aplicacion de Gastos')
 
 
+        # Frame 
+        frame = LabelFrame(self.wind, text = 'Registra un nuevo gasto')
+        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+
+        # Label
+        Label(frame, text = 'Nombre: ').grid(row = 1, column = 0)
+        self.name = Entry(frame)
+        self.name.focus()
+        self.name.grid(row = 1, column = 1)
+
+
+
 
 if __name__ == '__main__':
     window = Tk()
